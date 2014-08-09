a7844ef6871fe0d9a1691d3db4230e585ca7c668
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2014-08-09 15:34:40 -0400

    data model

--- InstaBT.Api/Library.fs
+++ InstaBT.Api/Library.fs
@@ -1,9 +1,21 @@
 ﻿namespace InstaBT.Api
 
+open System.Text
+
 open Romeo.Common
 open Romeo.Client
 
+open Newtonsoft.Json
+
 type Merchant(url : string, api_key : string, api_secret : string ) =
-  let config = { server_url = url; api_key = api_key; app_secret = api_secret; debug = false}
+
+  let config = 
+    { server_url = url
+    ; api_key = api_key
+    ; app_secret = api_secret
+    ; debug = false }
+
   member this.CreateOrder(amount : decimal, currency : string ) = 
-   call config "/create_order" [| ("amount", amount.ToString()); ("currency", currency)|]
+
+   let bytes = call config "/create_order" [| ("amount", amount.ToString()); ("currency", currency)|]
+   JsonConvert.DeserializeObject<Model.MerchantOrder>(Encoding.UTF8.GetString(bytes))
---
+++ InstaBT.Api/Model.fs
@@ -0,0 +1,34 @@
+﻿namespace InstaBT.Api.Model
+
+open System
+open System.Runtime.Serialization
+
+
+[<DataContract>]
+type MerchantOrder =
+  { 
+  [<field: DataMember(Name = "id")>]
+  id   : Guid;
+  [<field: DataMember(Name = "total")>]
+  total    : decimal;
+  [<field: DataMember(Name = "currency")>]
+  currency       : string;
+  [<field: DataMember(Name = "btc_required")>]
+  btc_required    : decimal;
+  [<field: DataMember(Name = "btc_current")>]
+  btc_current    : decimal;
+  [<field: DataMember(Name = "btc_address")>]
+  btc_address     : string;
+  [<field: DataMember(Name = "data")>]
+  data  : string;
+  [<field: DataMember(Name = "created_on")>]
+  created_on    : DateTime;
+  [<field: DataMember(Name = "expires_on")>]
+  expires_on    : DateTime;
+  [<field: DataMember(Name = "status")>]
+  status    : string;
+  [<field: DataMember(Name = "last_update")>]
+  last_update    : DateTime;
+  [<field: DataMember(Name = "url")>]
+  url    : string;
+  }
