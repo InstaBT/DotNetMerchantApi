d72ee47e9893535fe54526e010c9d83153e9fcec
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2014-08-09 14:30:59 -0400

    first bits

---
+++ InstaBT.Api/Library.fs
@@ -0,0 +1,9 @@
+﻿namespace InstaBT.Api
+
+open Romeo.Common
+open Romeo.Client
+
+type Merchant(url : string, api_key : string, api_secret : string ) =
+  let config = { server_url = url; api_key = api_key; app_secret = api_secret; debug = false}
+  member this.CreateOrder(amount : decimal, currency : string ) = 
+   call config "/create_order" [| ("amount", amount.ToString()); ("currency", currency)|]
