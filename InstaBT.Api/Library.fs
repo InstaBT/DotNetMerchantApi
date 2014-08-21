b954ef7cfc1bb126c5a91db9a3d877682214a3fb
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2014-08-21 15:42:09 -0400

    optional parameters

--- InstaBT.Api/Library.fs
+++ InstaBT.Api/Library.fs
@@ -15,7 +15,41 @@ type Merchant(url : string, api_key : string, api_secret : string ) =
     ; app_secret = api_secret
     ; debug = false }
 
+  let mutable data  : string  = ""
+  let mutable priority : int = 0;
+  let mutable ttl : int = 15;
+
+  let mutable url_call_back = ""
+  let mutable url_success = ""
+  let mutable url_failure = ""
+
   member this.CreateOrder(amount : decimal, currency : string ) = 
 
+   let bytes = 
+     call config "/create_order" 
+       [| ("amount", amount.ToString())
+        ; ("currency", currency)
+        ; ("data", data) 
+        ; ("priority", priority.ToString()) 
+        ; ("ttl", ttl.ToString()) 
+        ; ("url_call_back", url_call_back) 
+        ; ("url_success", url_success) 
+        ; ("url_failure", url_failure) |]
+
+   let str = Encoding.UTF8.GetString bytes
+
+   JsonConvert.DeserializeObject<Model.MerchantOrder> str
+
+  member this.CreateOrderString(amount : decimal, currency : string ) = 
+
    let bytes = call config "/create_order" [| ("amount", amount.ToString()); ("currency", currency)|]
-   JsonConvert.DeserializeObject<Model.MerchantOrder>(Encoding.UTF8.GetString(bytes))
+
+   Encoding.UTF8.GetString bytes
+
+  member x.Data with get() = data and set(v) = data <- v
+  member x.Priority with get() = priority and set(v) = priority <- v
+  member x.Ttl with get() = ttl and set(v) = ttl <- v
+
+  member x.UrlCallback with get() = url_call_back and set(v) = url_call_back <- v
+  member x.UrlSuccess with get() = url_success and set(v) = url_success <- v
+  member x.UrlFailure with get() = url_failure and set(v) = url_failure <- v
